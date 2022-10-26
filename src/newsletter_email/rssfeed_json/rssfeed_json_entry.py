fc115b564152a2c2497a4aa4d71dfccabe511f45
Andrej Maya <a.maya@reply.de>
2022-10-26 09:42:35 +0200

    set dummy value for startDateTime

--- src/newsletter_email/rssfeed_json/rssfeed_json_entry.py
+++ src/newsletter_email/rssfeed_json/rssfeed_json_entry.py
@@ -8,11 +8,10 @@ class rssfeed_json_entry:
   """
 
   def __init__(self, entry):
-    if 'startDateTime' in entry:
-      self.title = entry['headline']
-      self.link = entry['headlineUrl']
-      self.level = entry['expertise']
-      #e.g. 2022-10-06T20:39:20+00:00
-      self.published = datetime.strptime(entry['startDateTime'], '%Y-%m-%dT%X%z').date()
-    else:
-      self.published = "1970-10-06T20:39:20+00:00"
+    #e.g. entry['startDateTime'] == "2022-10-06T20:39:20+00:00"
+    startDateTime = entry['startDateTime'] if 'startDateTime' in entry else "1970-01-01T00:00:00+00:00"
+
+    self.title = entry['headline']
+    self.link = entry['headlineUrl']
+    self.level = entry['expertise']
+    self.published = datetime.strptime(startDateTime, '%Y-%m-%dT%X%z').date()
