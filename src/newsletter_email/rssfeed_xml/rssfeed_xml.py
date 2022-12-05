ba6824d29716ae7e3c51ee826fe40bdec0172b08
Andrej Maya <a.maya@reply.de>
2022-12-05 18:24:51 +0100

    (fix) indent error in rss_entry py

--- src/newsletter_email/rssfeed_xml/rssfeed_xml.py
+++ src/newsletter_email/rssfeed_xml/rssfeed_xml.py
@@ -2,20 +2,20 @@ import feedparser, importlib
 
 
 class rssfeed_xml:
-   """
+  """
     title: str
     link: str
     entries: []  
   """
 
-    def __init__(self, feed, cutoff_date):
-        self.link = feed['link']
+  def __init__(self, feed, cutoff_date):
+    self.link = feed['link']
 
-        feed_output = feedparser.parse(self.link)
-        self.title = feed_output.feed.title
-        self.entries = []
-        for entry in feed_output.entries:
-            module = importlib.import_module('rssfeed_xml.' + feed['class_name'])
-            class_ = getattr(module, feed['class_name'])
-            entry_obj = class_(entry)
-            self.entries.append(entry_obj) if entry_obj.published > cutoff_date else None
+    feed_output = feedparser.parse(self.link)
+    self.title = feed_output.feed.title
+    self.entries = []
+    for entry in feed_output.entries:
+        module = importlib.import_module('rssfeed_xml.' + feed['class_name'])
+        class_ = getattr(module, feed['class_name'])
+        entry_obj = class_(entry)
+        self.entries.append(entry_obj) if entry_obj.published > cutoff_date else None
