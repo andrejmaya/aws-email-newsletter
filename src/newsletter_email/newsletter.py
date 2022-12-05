bed4e9ccfd96dcffef6911640485f402039fa0a6
kobbiomar <47794950+kobbiomar@users.noreply.github.com>
2022-12-05 18:05:07 +0100

    Email new seletter dev (#1)
    * update product filed to category entry
    * refactoring
    
    Co-authored-by: o.kobbi <o.kobbi@reply.de>

--- src/newsletter_email/newsletter.py
+++ src/newsletter_email/newsletter.py
@@ -8,78 +8,83 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 with open("rssfeed_xml/feedlist.json", "r") as feed_list_xml_f:
-  feed_list_xml = json.load(feed_list_xml_f)
+    feed_list_xml = json.load(feed_list_xml_f)
 with open("rssfeed_json/feedlist.json", "r") as feed_list_json_f:
-  feed_list_json = json.load(feed_list_json_f)
+    feed_list_json = json.load(feed_list_json_f)
+
 
 def main(event, context):
-  ses_client = boto3.client('ses')
-  subscriptions = get_subscribers()
-  cutoff_date = (datetime.now()-timedelta(days=int(os.environ['CUTOFF_DAYS']))).date()
-  logging.info(f"subscriptions:{subscriptions}")
-  
-  feeds = [rssfeed_xml(feed, cutoff_date) for feed in feed_list_xml]
-  feeds += [rssfeed_json(feed, cutoff_date) for feed in feed_list_json]
+    ses_client = boto3.client('ses')
+    subscriptions = get_subscribers()
+    cutoff_date = (datetime.now() - timedelta(days=int(os.environ['CUTOFF_DAYS']))).date()
+    logging.info(f"subscriptions:{subscriptions}")
 
-  mail_body_part = render_mail(
-    feeds=feeds, 
-    template="body.jinja"
-  )
+    feeds = [rssfeed_xml(feed, cutoff_date) for feed in feed_list_xml]
+    feeds += [rssfeed_json(feed, cutoff_date) for feed in feed_list_json]
 
-  for subscriber in subscriptions:
-    endpoint = subscriber['Endpoint']
-
-    mail_body = mail_body_part + render_mail(
-      SubscriptionArn=subscriber['SubscriptionArn'],
-      template="footer.jinja"
+    mail_body_part = render_mail(
+        feeds=feeds,
+        template="body.jinja"
     )
-    
-    logging.info(f"endpoint:{endpoint}, mail_body:{mail_body}")
-    response = ses_client.send_email(
-        Source=os.environ['EMAIL_SENDER'],
-        Destination={
-            'ToAddresses': [
-                endpoint,
-            ]
-        },
-        Message={
-            'Subject': {
-                'Data': 'AWS Roadmap Items of Interest',
+
+    for subscriber in subscriptions:
+        endpoint = subscriber['Endpoint']
+
+        mail_body = mail_body_part + render_mail(
+            SubscriptionArn=subscriber['SubscriptionArn'],
+            template="footer.jinja"
+        )
+
+        logging.info(f"endpoint:{endpoint}, mail_body:{mail_body}")
+        response = ses_client.send_email(
+            Source=os.environ['EMAIL_SENDER'],
+            Destination={
+                'ToAddresses': [
+                    endpoint,
+                ]
             },
-            'Body': {
-                'Html': {
-                    'Data': mail_body,
-                    'Charset': 'UTF-8'
+            Message={
+                'Subject': {
+                    'Data': 'AWS Roadmap Items of Interest',
+                },
+                'Body': {
+                    'Html': {
+                        'Data': mail_body,
+                        'Charset': 'UTF-8'
+                    }
                 }
             }
-        }
-    )
+        )
+
 
 def get_subscribers():
-  sns_client = boto3.client('sns')
-  response = sns_client.list_subscriptions_by_topic(
-      TopicArn=os.environ['SNS_SUBSCRIBERS_ARN']
-  )
-  subscriptions = get_active_subscribers(response['Subscriptions'])
-  while "NextToken" in response:
-      response = sns_client.list_subscriptions_by_topic(
-          TopicArn=os.environ['SNS_SUBSCRIBERS_ARN'],
-          NextToken=response["NextToken"]
-      )  
-      subscriptions.extend(get_active_subscribers(response['Subscriptions']))
-  return subscriptions
+    sns_client = boto3.client('sns')
+    response = sns_client.list_subscriptions_by_topic(
+        TopicArn=os.environ['SNS_SUBSCRIBERS_ARN']
+    )
+    subscriptions = get_active_subscribers(response['Subscriptions'])
+    while "NextToken" in response:
+        response = sns_client.list_subscriptions_by_topic(
+            TopicArn=os.environ['SNS_SUBSCRIBERS_ARN'],
+            NextToken=response["NextToken"]
+        )
+        subscriptions.extend(get_active_subscribers(response['Subscriptions']))
+    return subscriptions
+
 
 def get_active_subscribers(resp_subscriptions):
-  return [obj for obj in resp_subscriptions if obj['SubscriptionArn'].startswith('arn:aws:sns')]
+    return [obj for obj in resp_subscriptions if obj['SubscriptionArn'].startswith('arn:aws:sns')]
+
 
 def render_mail(**kwargs):
-  environment = Environment(loader=FileSystemLoader("email_template/"))
-  template = environment.get_template(kwargs.get("template"))
+    environment = Environment(loader=FileSystemLoader("email_template/"))
+    template = environment.get_template(kwargs.get("template"))
 
-  return template.render(
-    kwargs,
-    Region=boto3.session.Session().region_name
-  )
+    return template.render(
+        kwargs,
+        Region=boto3.session.Session().region_name
+    )
+
 
 if __name__ == "__main__":
-    main({},{})
+    main({}, {})
