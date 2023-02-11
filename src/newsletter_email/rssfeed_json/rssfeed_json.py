d32e363a28ee054d3e15fd2de369f62ee167e2ba
Andrej Maya <andrej.maya@nttdata.com>
2023-02-11 18:14:21 +0100

    fix(json-parser): fix online tech talks JSON

--- src/newsletter_email/rssfeed_json/rssfeed_json.py
+++ src/newsletter_email/rssfeed_json/rssfeed_json.py
@@ -1,10 +1,13 @@
 import importlib, urllib3, json
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 class rssfeed_json:
   """
     title: str
     link: str
-    entries: []  
+    entries: []
   """
 
   def __init__(self, feed, cutoff_date):
@@ -14,11 +17,12 @@ class rssfeed_json:
 
     resp = json.loads(resp.data.decode('utf-8'))
 
-    
+
     self.entries = []
 
     for entry in resp['items']:
       module = importlib.import_module('rssfeed_json.'+feed['class_name'])
       class_ = getattr(module, feed['class_name'])
       entry_obj = class_(entry['item']['additionalFields'])
+      logging.info(f"entry_obj.published:{entry_obj.published} > cutoff_date:{cutoff_date}?")
       self.entries.append(entry_obj) if entry_obj.published > cutoff_date else None
--- src/newsletter_email/rssfeed_json/rssfeed_json_entry.py
+++ src/newsletter_email/rssfeed_json/rssfeed_json_entry.py
@@ -1,17 +1,21 @@
 from datetime import datetime, date
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 class rssfeed_json_entry:
   """
     title: str
     link: str
-    published: date  
+    published: date
   """
 
   def __init__(self, entry):
     #e.g. entry['startDateTime'] == "2022-10-06T20:39:20+00:00"
-    startDateTime = entry['startDateTime'] if 'startDateTime' in entry else "1970-01-01T00:00:00+00:00"
+    logging.info(f"rssfeed_json_entry:{entry}")
+    startDateTime = entry['startDate'] if 'startDate' in entry else "1970-01-01T00:00:00+00:00"
 
     self.title = entry['headline']
     self.link = entry['headlineUrl']
     self.level = entry['expertise']
-    self.published = datetime.strptime(startDateTime, '%Y-%m-%dT%X%z').date()
+    self.published = datetime.strptime(startDateTime,'%d-%b-%y').date()
